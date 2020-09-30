7b107f473892d572caed02ffb672cb486d354ef6
Victoria Landron <viclandron@gmail.com>
2020-09-29 20:49:05 -0400

    prompt user for more info

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i+= 1) {
-        console.log(profileDaraArr[i]);
-    }
+const generatePage = require('./src/page-template.js');
 
-    console.log('==============');
+const profileDataArgs = process.argv.slice(2);
 
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+const [name, github] = profileDataArgs;
 
-printProfileData(profileDataArgs);
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+})
