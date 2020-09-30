af1a1215af00317dd868a5571dd1ba7f5260ceaa
Victoria Landron <viclandron@gmail.com>
2020-09-29 20:51:54 -0400

    CORRECTION: last commit we created the html file

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const profileDataArgs = process.argv.slice(2);
 
 const [name, github] = profileDataArgs;
 
+
 fs.writeFile('./index.html', generatePage(name, github), err => {
     if (err) throw new Error(err);
 
