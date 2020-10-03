65cf920209f688506b86c74610cc75fe647d0b40
Victoria Landron <viclandron@gmail.com>
2020-10-03 18:37:33 -0400

    modifying the HTML to accept and print more data

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
 const inquirer = require('inquirer');
 
 const promptUser = () => {
@@ -143,16 +146,14 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+        const pageHTML = generatePage(portfolioData);
+
+        fs.writeFile('./index.html', pageHTML, err => {
+         if (err) throw err;
+
+         console.log('Portfolio complete! Check out index.html to see the output!');
+        })
     });
     
-//const fs = require('fs');
-//const generatePage = require('./src/page-template.js');
 
-//const pageHTML = generatePage(name, github);
 
-//fs.writeFile('./index.html', pageHTML, err => {
-  //  if (err) throw err;
-
-  //  console.log('Portfolio complete! Check out index.html to see the output!');
-//})
