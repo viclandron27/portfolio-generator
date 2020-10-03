67bec231e62feee6276101609d4a9a7e8ce3493f
Victoria Landron <viclandron@gmail.com>
2020-10-03 19:21:20 -0400

    generate webstie with stylesheet and promises

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 const generatePage = require('./src/page-template.js');
 
 const inquirer = require('inquirer');
@@ -146,13 +146,20 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        const pageHTML = generatePage(portfolioData);
-
-        fs.writeFile('./index.html', pageHTML, err => {
-         if (err) throw err;
-
-         console.log('Portfolio complete! Check out index.html to see the output!');
-        })
+        return generatePage(portfolioData);
+    })
+    .then(pageHTML => {
+        return writeFile(pageHTML);
+    })
+    .then(writeFileResponse => {
+        console.log(writeFileResponse);
+        return copyFile();
+    })
+    .then(copyFileResponse => {
+        console.log(copyFileResponse);
+    })
+    .catch(err => {
+        console.log(err);
     });
     
 
